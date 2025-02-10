85f58555cf8260094bcda8fd947b74cfb3608b23
pepperpig183-sketch <pepperandsaltpig@gmail.com>
2025-02-10 16:58:50 +0000

    desktop change heavy

--- components/Desktop.js
+++ components/Desktop.js
@@ -18,46 +18,13 @@ const Desktop = (props) => {
           setRegion={props.setRegion}
         />
       )}
-      <a
-        href="https://alyssax.substack.com/p/we-built-an-ai-recycling-app-in-a"
-        target="_blank"
-        className={styles.awards}
-      >
-        
-      </a>
-      <div className={styles.badge}>
-        <img src="logo-with-back.svg" />
-        <div className={styles.badgewrap}>
-          <div className={styles.name}>EcoSnap</div>
-          <div className={styles.credit}>
-            Made by{" "}
-            <a href="https://www.linkedin.com/in/leonorfurtado" target="_blank">
-              Leo
-            </a>{" "}
-            &{" "}
-            <a href="https://twitter.com/alyssaxuu/" target="_blank">
-              Alyssa X
-            </a>
-          </div>
-        </div>
-      </div>
-      <a
-        href="https://twitter.com/intent/tweet?text=Check%20out%20EcoSnap%20to%20learn%20how%20to%20recycle%20your%20plastic%20better%20with%20AI&url=http%3A%2F%2Fecosnap.vercel.app"
-        className={styles.share}
-        target="_blank"
-      >
-        <img src="twitter.svg" /> Share
-      </a>
-      <a
-        href="https://github.com/alyssaxuu/ecosnap"
-        target="_blank"
-        className={styles.github}
-      >
-        <img src="github.svg" /> View on GitHub
-      </a>
+     
+      
+      
+      
       <div className={styles.qrcode}>
         <img src="qrcode.png" />
-        <span>Open on your phone ✨</span>
+        <span>Open on your phone (update this) ✨</span>
       </div>
     </div>
   );
