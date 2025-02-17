a518b4b99f315a3cf8b7de8ea677d84788693bf6
pepperpig183-sketch <pepperandsaltpig@gmail.com>
2025-02-17 06:03:44 +0000

    different logo?

--- components/Splash.js
+++ components/Splash.js
@@ -68,7 +68,7 @@ const Splash = (props) => {
 			{!getStarted &&
 			<div className={styles.splash}>
 				<div className={styles.logo}>
-					<img src="logo_white1.svg"/>
+					<img src="logo_white2.svg"/>
 					sustAInably
 				</div>
 				<div className={styles.illustration}>
