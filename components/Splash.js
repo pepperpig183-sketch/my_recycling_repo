91f7ff88cd84e97cabea8029be7424faf818ea98
pepperpig183-sketch <pepperandsaltpig@gmail.com>
2025-02-17 05:51:50 +0000

    new logo

--- components/Splash.js
+++ components/Splash.js
@@ -68,7 +68,7 @@ const Splash = (props) => {
 			{!getStarted &&
 			<div className={styles.splash}>
 				<div className={styles.logo}>
-					<img src="logo_white.jpeg"/>
+					<img src="logo_white1.svg"/>
 					sustAInably
 				</div>
 				<div className={styles.illustration}>
